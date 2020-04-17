bc009383c68000a13a6d185deb9fe5a4b798b8bd
zshureih <42466551+zshureih@users.noreply.github.com>
2020-04-16 19:20:50 -0700

    bfs is working

--- chickenWolfSolver.py
+++ chickenWolfSolver.py
@@ -2,8 +2,8 @@ import sys
 
 class riverBank:
     def __init__(self, chickens, wolves, boat):
-        self.wolves = wolves #number of wolves on bank
         self.chickens = chickens #number of chickens on bank
+        self.wolves = wolves #number of wolves on bank
         self.boat = boat #boolean flag for if boat is on bank
     
     # if returns 0, bank is unbalanced, player loses
@@ -23,17 +23,26 @@ class Game:
     def checkLoss(self):
         # if both banks are still in balance, player has not lost
         if self.leftBank.checkBalance() and self.rightBank.checkBalance():
-            return 0
+            return 
         else:
             return 1
 
     def checkWin(self):
         currentState = self.getCurrentState()
 
-        if currentState == self.goalState:
-            return 1
-        else:
+        if currentState[0][0] != self.goalState[0][0]:
             return 0
+        if currentState[0][1] != self.goalState[0][1]:
+            return 0
+        if currentState[0][2] != self.goalState[0][2]:
+            return 0
+        if currentState[1][0] != self.goalState[1][0]:
+            return 0
+        if currentState[1][1] != self.goalState[1][1]:
+            return 0
+        if currentState[1][1] != self.goalState[1][1]:
+            return 0
+        return 1
 
     def getCurrentState(self):
         return [[self.leftBank.chickens, self.leftBank.wolves, self.leftBank.boat],
@@ -44,9 +53,9 @@ class Game:
         self.leftBank.chickens = state[0][0]
         self.leftBank.wolves = state[0][1]
         self.leftBank.boat = state[0][2]
-        self.leftBank.chickens = state[1][0]
-        self.leftBank.wolves = state[1][1]
-        self.leftBank.boat = state[1][2]
+        self.rightBank.chickens = state[1][0]
+        self.rightBank.wolves = state[1][1]
+        self.rightBank.boat = state[1][2]
 
     def oneChickenAction(self, startBank, destBank):
         # save current state for undo
@@ -120,36 +129,47 @@ class Game:
         self.rightBank.wolves = self.lastState[1][1]
         self.rightBank.boat = self.lastState[1][2]
 
-        self.lastState = 0
+
+    def possibleState(self):
+        currentState = self.getCurrentState()
+
+        if currentState[0][0] + currentState[1][0] > 3 or currentState[0][0] < 0 or currentState[1][0] < 0:
+            return 0
+        if currentState[0][1] + currentState[1][1] > 3 or currentState[0][1] < 0 or currentState[1][1] < 0:
+            return 0
+        if currentState[0][2] + currentState[1][2] > 1 or currentState[0][2] < 0 or currentState[1][2] < 0:
+            return 0
+
+        return 1 
 
     def getFrontier(self):
         frontier = []
-
+        # print(self.getCurrentState())
         if self.leftBank.boat:
             successor = 0
 
             successor = self.oneChickenAction(self.leftBank, self.rightBank)
-            if not self.checkLoss():
+            if not self.checkLoss() and self.possibleState():
                 frontier.append(successor)
             self.undo()
 
             successor = self.twoChickenAction(self.leftBank, self.rightBank)
-            if not self.checkLoss():
+            if not self.checkLoss() and self.possibleState():
                 frontier.append(successor)
             self.undo()
 
             successor = self.oneWolfAction(self.leftBank, self.rightBank)
-            if not self.checkLoss():
+            if not self.checkLoss() and self.possibleState():
                 frontier.append(successor)
             self.undo()
 
             successor = self.twoWolfAction(self.leftBank, self.rightBank)
-            if not self.checkLoss():
+            if not self.checkLoss() and self.possibleState():
                 frontier.append(successor)
             self.undo()
 
             successor = self.oneEachAction(self.leftBank, self.rightBank)
-            if not self.checkLoss():
+            if not self.checkLoss() and self.possibleState():
                 frontier.append(successor)
             self.undo()
 
@@ -157,30 +177,31 @@ class Game:
             successor = 0
 
             successor = self.oneChickenAction(self.rightBank, self.leftBank)
-            if not self.checkLoss():
+            if not self.checkLoss() and self.possibleState():
                 frontier.append(successor)
             self.undo()
 
             successor = self.twoChickenAction(self.rightBank, self.leftBank)
-            if not self.checkLoss():
+            if not self.checkLoss() and self.possibleState():
                 frontier.append(successor)
             self.undo()
 
             successor = self.oneWolfAction(self.rightBank, self.leftBank)
-            if not self.checkLoss():
+            if not self.checkLoss() and self.possibleState():
                 frontier.append(successor)
             self.undo()
 
             successor = self.twoWolfAction(self.rightBank, self.leftBank)
-            if not self.checkLoss():
+            if not self.checkLoss() and self.possibleState():
                 frontier.append(successor)
             self.undo()
 
             successor = self.oneEachAction(self.rightBank, self.leftBank)
-            if not self.checkLoss():
+            if not self.checkLoss() and self.possibleState():
                 frontier.append(successor)
             self.undo()
-                
+        
+        # print(frontier)
         return frontier
 
 def parseInputs(initialStateFile, goalStateFile):
@@ -203,26 +224,30 @@ def bfsSolver(initialStateFile, goalStateFile, outputFile):
     initState, goalState = parseInputs(initialStateFile, goalStateFile)
 
     game = Game(goalState, initState)
-    print(game.getCurrentState())
     #initialize search problem
     explored = []
     frontier = game.getFrontier()
     while(1):
         if len(frontier) == 0:
-            return False
+            return game.getCurrentState()
         
         child = frontier.pop()
+        # print(frontier)
+        # print(child)
         game.setCurrentState(child)
+        # print(game.getCurrentState())
 
         if game.checkWin():
             return explored
-            
-        explored.append(game.getCurrentState())
-    
+        
+        explored.append(child)
+        # print(explored)
         successors = game.getFrontier()
         for s in successors:
-            if not s in frontier:
+            if not s in frontier and not s in explored:
                 frontier.append(s)
+        # print(frontier)
+        
         
 def dfsSolver(initialStateFile, goalStateFile, outputFile):
     print("hello world")
@@ -234,6 +259,7 @@ def aStarSolver(initialStateFile, goalStateFile, outputFile):
     print("hello world")
 
 def main():
+    result = 0
     if sys.argv[3] == "bfs":
         result = bfsSolver(sys.argv[1], sys.argv[2], sys.argv[4])
     if sys.argv[3] == "dfs":
